e6aad798c8bf95354b900844c38ce28ac68a9fb6
bronewi4ok <bronewi4ok@gmail.com>
2021-04-08 13:22:38 +0300

    fixed 79-85

--- js/script.js
+++ js/script.js
@@ -1,44 +1,51 @@
-let badges = document.querySelectorAll('.kv-buch-chb__badge');
-for (const badge of badges) {
-    badge.addEventListener('click', (event) => {
-        console.log('OLOL');
-        event.stopPropagation();
-        event.preventDefault();
-        console.log('ZAZAZAZ');
-    });
+function resizeElement(containerClass, elmentClass, addClass, breakpoint) {
+    let containers = document.querySelectorAll(containerClass);
+    let elements;
+
+    if (containers) {
+        for (let i = 0; i < containers.length; i++) {
+            let container = containers[i];
+
+            if (containerClass === elmentClass || !elmentClass) {
+                elements = document.querySelectorAll(containerClass);
+            } else {
+                elements = container.querySelectorAll(elmentClass);
+            }
+
+            if (elements) {
+                for (let j = 0; j < elements.length; j++) {
+                    let element = elements[j];
+
+                    if (container.offsetWidth <= breakpoint) {
+                        element.classList.add(addClass);
+                    }
+                    else if (element.classList.contains(addClass)) {
+                        element.classList.remove(addClass);
+                    }
+                }
+            }
+        }
+    }
 }
 
+['resize', 'load'].forEach(evt =>
+    window.addEventListener(evt, () => {
+        resizeElement('.kv-class-tabs', '.kv-class-tab', 'kv-class-tab_bp', 991);
+        resizeElement('.kv-class-tab', '.kv-class-tab__title', 'kv-class-tab__title_hidden', 94);
+        resizeElement('.kv-services', '.kv-classes__item', 'kv-classes__item_bp', 991);
+        resizeElement('.kv-services', '.kv-classes', 'kv-classes_bp', 991);
+        resizeElement('.kv-services', '.kv-class', 'kv-class_bp', 991);
+        resizeElement('.kv-services', '.kv-services', 'kv-services_bp', 991);
+        resizeElement('.kv-services', '.kv-services__item', 'kv-services__item_bp', 991);
+        resizeElement('.kv-class-tabs', '.kv-tabs-tail', 'kv-tabs-tail_bp', 991);
+        resizeElement('.kv-class-tabs', '.kv-class-tabs', 'kv-class-tabs_bp', 991);
+        resizeElement('.kv-services__footer', '.kv-services__prices', 'kv-services__prices_bp', 991);
 
-// MODULES
-// include('./modules/_textarea.js');
-// include('./modules/_da.js');
-// include('./modules/_sliders.js');
-// include('./modules/_media.js');
-// MODULES
 
-// UI
-// include('./ui/_livesearch.js');
-// include('./ui/_select.js');
-// include('./ui/_scroll.js');
-// include('./ui/_tippy.js');
-// include('./ui/_drop.js');
-// include('./ui/_accordeon.js');
-// include('./ui/_tabs.js');
-// include('./ui/_file-input.js');
-// include('./ui/_popup.js');
-// include('./ui/_mask.js');
-// include('./ui/_validation.js');
-// include('./ui/_show-pass.js');
-// include('./ui/_ibg.js');
-// include('./ui/_lazy.js');
-// include('./ui/_colorpicker.js');
-// include('./ui/_dashboard.js');
-// include('./ui/_preload.js');
-// include('./ui/_toast.js');
-// UI
+    }, false)
+);
 
-// COMPONENTS
-// include('./components/_header.js');
-// COMPONENTS
 
-// include('./modules/_aos_script.js');
+
+
+
